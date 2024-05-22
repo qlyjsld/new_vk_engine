0ced9b620a2791d16d9d98333f44335a17e8e31f
Hoi Pok Wu <wuhoipok@gmail.com>
2024-05-22 14:03:06 -0400

    some optimisation and refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,8 @@ struct camera_data {
 };
 
 struct cloud_data {
-    alignas(4) float inner_radius;
+    alignas(4) float inner;
     alignas(4) float height;
-    alignas(4) int cloudtex_size;
-    alignas(4) int weather_size;
     alignas(4) float type;
     alignas(4) float freq;
     alignas(4) float ambient;
@@ -32,9 +30,9 @@ struct cloud_data {
     alignas(4) int max_steps;
     alignas(4) float cutoff;
     alignas(4) float density;
-    alignas(4) float lambda;
-    alignas(4) float temperature;
-    alignas(16) glm::vec3 color;
+
+    alignas(4) int cloudtex_size;
+    alignas(4) int weather_size;
 };
 
 /*
@@ -43,11 +41,11 @@ struct cloud_data {
     cloud appears at above = 1500m;
  */
 
-static float cloud_inner_radius = 0.f + 150.f;
+static float cloud_inner = 0.f + 150.f;
 static float cloud_height = 900.f - 150.f;
 static uint32_t cloudtex_size = 128;
 static uint32_t weather_size = 512;
-static bool cloud = true;
+static bool cloud_ui = true;
 static cloud_data cloud_data;
 
 int main(int argc, char *argv[])
@@ -63,84 +61,6 @@ int main(int argc, char *argv[])
     engine.run();
     engine.cleanup();
     return 0;
-}
-
-void vk_engine::skybox_init()
-{
-    comp_allocator allocator(_device, _allocator);
-
-    allocator.create_buffer(pad_uniform_buffer_size(sizeof(camera_data)),
-                            VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
-                            VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT, "camera");
-
-    allocator.create_buffer(pad_uniform_buffer_size(sizeof(glm::vec2)),
-                            VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
-                            VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT, "extent");
-
-    glm::vec2 extent = glm::vec2{
-        _resolution.width,
-        _resolution.height,
-    };
-
-    allocated_buffer buffer = allocator.get_buffer("extent");
-
-    void *data;
-    vmaMapMemory(_allocator, buffer.allocation, &data);
-    std::memcpy(data, &extent, sizeof(glm::vec2));
-    vmaUnmapMemory(_allocator, buffer.allocation);
-
-    allocator.load_img("target", _target);
-
-    std::vector<descriptor> descriptors = {
-        {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, "target"},
-        {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, "extent"},
-        // {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, "camera"},
-    };
-
-    cs skybox(allocator, descriptors, "../shaders/skybox.comp.spv",
-              _min_buffer_alignment);
-
-    PipelineBuilder pb = {};
-    pb._shader_stage_infos.push_back(
-        vk_boiler::shader_stage_create_info(VK_SHADER_STAGE_COMPUTE_BIT, skybox.module));
-
-    std::vector<VkDescriptorSetLayout> layouts = {
-        skybox.layout,
-    };
-
-    std::vector<VkPushConstantRange> push_constants = {};
-
-    pb.build_comp(_device, layouts, push_constants, &skybox.pipeline_layout,
-                  &skybox.pipeline);
-
-    skybox.draw = [=](VkCommandBuffer cbuffer, cs *cs) {
-        vkCmdBindPipeline(cbuffer, VK_PIPELINE_BIND_POINT_COMPUTE, cs->pipeline);
-
-        /* camera_data camera_data;
-        camera_data.pos = _vk_camera.get_pos();
-        camera_data.dir = _vk_camera.get_dir();
-        camera_data.up = _vk_camera.get_up();
-        camera_data.fov = _vk_camera.get_fov();
-
-        void *data;
-        vmaMapMemory(_allocator, cs->allocator.get_buffer("camera").allocation, &data);
-        std::memcpy(data, &camera_data, pad_uniform_buffer_size(sizeof(camera_data)));
-        vmaUnmapMemory(cs->allocator.allocator,
-                       cs->allocator.get_buffer("camera").allocation); */
-
-        std::vector<uint32_t> doffsets = {
-            0,
-            // 0,
-        };
-
-        vkCmdBindDescriptorSets(cbuffer, VK_PIPELINE_BIND_POINT_COMPUTE,
-                                cs->pipeline_layout, 0, 1, &cs->set, doffsets.size(),
-                                doffsets.data());
-
-        vkCmdDispatch(cbuffer, _resolution.width / 8, _resolution.height / 8, 1);
-    };
-
-    // css.push_back(skybox);
 }
 
 void vk_engine::cloudtex_init()
@@ -281,50 +201,6 @@ void vk_engine::weather_init()
     cs::comp_immediate_submit(_device, _comp_queue, &weather);
 }
 
-void vk_engine::sphere_init()
-{
-    comp_allocator allocator(_device, _allocator);
-
-    std::vector<descriptor> descriptors = {
-        {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, "target"},
-        {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, "extent"},
-        {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, "camera"},
-    };
-
-    cs sphere(allocator, descriptors, "../shaders/sphere.comp.spv",
-              _min_buffer_alignment);
-
-    PipelineBuilder pb = {};
-    pb._shader_stage_infos.push_back(
-        vk_boiler::shader_stage_create_info(VK_SHADER_STAGE_COMPUTE_BIT, sphere.module));
-
-    std::vector<VkDescriptorSetLayout> layouts = {
-        sphere.layout,
-    };
-
-    std::vector<VkPushConstantRange> push_constants = {};
-
-    pb.build_comp(_device, layouts, push_constants, &sphere.pipeline_layout,
-                  &sphere.pipeline);
-
-    sphere.draw = [=](VkCommandBuffer cbuffer, cs *cs) {
-        vkCmdBindPipeline(cbuffer, VK_PIPELINE_BIND_POINT_COMPUTE, cs->pipeline);
-
-        std::vector<uint32_t> doffsets = {
-            0,
-            0,
-        };
-
-        vkCmdBindDescriptorSets(cbuffer, VK_PIPELINE_BIND_POINT_COMPUTE,
-                                cs->pipeline_layout, 0, 1, &cs->set, doffsets.size(),
-                                doffsets.data());
-
-        vkCmdDispatch(cbuffer, _resolution.width / 8, _resolution.height / 8, 1);
-    };
-
-    // css.push_back(sphere);
-}
-
 void vk_engine::cloud_init()
 {
     comp_allocator allocator(_device, _allocator);
@@ -333,7 +209,7 @@ void vk_engine::cloud_init()
                             VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
                             VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT, "cloud");
 
-    cloud_data.inner_radius = cloud_inner_radius;
+    cloud_data.inner = cloud_inner;
     cloud_data.height = cloud_height;
     cloud_data.cloudtex_size = cloudtex_size;
     cloud_data.weather_size = weather_size;
@@ -346,9 +222,6 @@ void vk_engine::cloud_init()
     cloud_data.max_steps = 64;
     cloud_data.cutoff = .3;
     cloud_data.density = 1.f;
-    cloud_data.lambda = 600.f;
-    cloud_data.temperature = 3000.f;
-    cloud_data.color = glm::vec3(1.f);
 
     std::vector<descriptor> descriptors = {
         {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, "target"},
@@ -414,7 +287,7 @@ void vk_engine::draw_comp(frame *frame)
 {
     ImGui::SetNextWindowSize(ImVec2{300, 290});
     ImGui::SetNextWindowPos(ImVec2{30, 30});
-    ImGui::Begin("cloud", &cloud, 0);
+    ImGui::Begin("cloud", &cloud_ui, 0);
     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
                 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
     ImGui::SliderFloat("type", &cloud_data.type, 0.f, 1.f);
@@ -426,9 +299,6 @@ void vk_engine::draw_comp(frame *frame)
     ImGui::SliderInt("max_steps", &cloud_data.max_steps, 0, 128);
     ImGui::SliderFloat("cutoff", &cloud_data.cutoff, 0.f, 1.f);
     ImGui::SliderFloat("density", &cloud_data.density, 0.f, 32.f);
-    // ImGui::SliderFloat("lambda", &cloud_data.lambda, 0.f, 1000.f);
-    // ImGui::SliderFloat("temperature", &cloud_data.temperature, 0.f, 10000.f);
-    // ImGui::ColorEdit3("color", (float *)&cloud_data.color);
     ImGui::End();
 
     vk_cmd::vk_img_layout_transition(frame->cbuffer, _target.img,
@@ -441,4 +311,126 @@ void vk_engine::draw_comp(frame *frame)
     vk_cmd::vk_img_layout_transition(frame->cbuffer, _target.img, VK_IMAGE_LAYOUT_GENERAL,
                                      VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
                                      _comp_index);
+}
+
+void vk_engine::skybox_init()
+{
+    comp_allocator allocator(_device, _allocator);
+
+    allocator.create_buffer(pad_uniform_buffer_size(sizeof(camera_data)),
+                            VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+                            VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT, "camera");
+
+    allocator.create_buffer(pad_uniform_buffer_size(sizeof(glm::vec2)),
+                            VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+                            VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT, "extent");
+
+    glm::vec2 extent = glm::vec2{
+        _resolution.width,
+        _resolution.height,
+    };
+
+    allocated_buffer buffer = allocator.get_buffer("extent");
+
+    void *data;
+    vmaMapMemory(_allocator, buffer.allocation, &data);
+    std::memcpy(data, &extent, sizeof(glm::vec2));
+    vmaUnmapMemory(_allocator, buffer.allocation);
+
+    allocator.load_img("target", _target);
+
+    std::vector<descriptor> descriptors = {
+        {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, "target"},
+        {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, "extent"},
+        // {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, "camera"},
+    };
+
+    cs skybox(allocator, descriptors, "../shaders/skybox.comp.spv",
+              _min_buffer_alignment);
+
+    PipelineBuilder pb = {};
+    pb._shader_stage_infos.push_back(
+        vk_boiler::shader_stage_create_info(VK_SHADER_STAGE_COMPUTE_BIT, skybox.module));
+
+    std::vector<VkDescriptorSetLayout> layouts = {
+        skybox.layout,
+    };
+
+    std::vector<VkPushConstantRange> push_constants = {};
+
+    pb.build_comp(_device, layouts, push_constants, &skybox.pipeline_layout,
+                  &skybox.pipeline);
+
+    skybox.draw = [=](VkCommandBuffer cbuffer, cs *cs) {
+        vkCmdBindPipeline(cbuffer, VK_PIPELINE_BIND_POINT_COMPUTE, cs->pipeline);
+
+        /* camera_data camera_data;
+        camera_data.pos = _vk_camera.get_pos();
+        camera_data.dir = _vk_camera.get_dir();
+        camera_data.up = _vk_camera.get_up();
+        camera_data.fov = _vk_camera.get_fov();
+
+        void *data;
+        vmaMapMemory(_allocator, cs->allocator.get_buffer("camera").allocation, &data);
+        std::memcpy(data, &camera_data, pad_uniform_buffer_size(sizeof(camera_data)));
+        vmaUnmapMemory(cs->allocator.allocator,
+                       cs->allocator.get_buffer("camera").allocation); */
+
+        std::vector<uint32_t> doffsets = {
+            0,
+            // 0,
+        };
+
+        vkCmdBindDescriptorSets(cbuffer, VK_PIPELINE_BIND_POINT_COMPUTE,
+                                cs->pipeline_layout, 0, 1, &cs->set, doffsets.size(),
+                                doffsets.data());
+
+        vkCmdDispatch(cbuffer, _resolution.width / 8, _resolution.height / 8, 1);
+    };
+
+    // css.push_back(skybox);
+}
+
+void vk_engine::sphere_init()
+{
+    comp_allocator allocator(_device, _allocator);
+
+    std::vector<descriptor> descriptors = {
+        {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, "target"},
+        {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, "extent"},
+        {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, "camera"},
+    };
+
+    cs sphere(allocator, descriptors, "../shaders/sphere.comp.spv",
+              _min_buffer_alignment);
+
+    PipelineBuilder pb = {};
+    pb._shader_stage_infos.push_back(
+        vk_boiler::shader_stage_create_info(VK_SHADER_STAGE_COMPUTE_BIT, sphere.module));
+
+    std::vector<VkDescriptorSetLayout> layouts = {
+        sphere.layout,
+    };
+
+    std::vector<VkPushConstantRange> push_constants = {};
+
+    pb.build_comp(_device, layouts, push_constants, &sphere.pipeline_layout,
+                  &sphere.pipeline);
+
+    sphere.draw = [=](VkCommandBuffer cbuffer, cs *cs) {
+        vkCmdBindPipeline(cbuffer, VK_PIPELINE_BIND_POINT_COMPUTE, cs->pipeline);
+
+        std::vector<uint32_t> doffsets = {
+            0,
+            0,
+        };
+
+        vkCmdBindDescriptorSets(cbuffer, VK_PIPELINE_BIND_POINT_COMPUTE,
+                                cs->pipeline_layout, 0, 1, &cs->set, doffsets.size(),
+                                doffsets.data());
+
+        vkCmdDispatch(cbuffer, _resolution.width / 8, _resolution.height / 8, 1);
+    };
+
+    // css.push_back(sphere);
 }
